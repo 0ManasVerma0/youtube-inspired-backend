745bbee9be0d2dd3a020ad1f04429eee75c5653f
Manas verma <manasverma1357@gmail.com>
2025-05-10 14:08:05 +0530

    Add models for comment,like,playlist,subscriptio,tweet,user,video

---
+++ src/models/subscription.models.js
@@ -0,0 +1,19 @@
+import mongoose, {Schema} from "mongoose";
+
+const subscriptionsSchema = new Schema(
+    {
+        subscriber: {
+            type: Schema.Types.ObjectId, //One who is subscribing
+            ref: "User"
+        },
+        channel: {
+            type: Schema.Types.ObjectId, //one to whom 'subscriber' is subscribing
+            ref: "User"
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Subscriptions = mongoose.model("Subscriptions", subscriptionsSchema)
